bf67c4b827d299ad723c01b7edf0ff7918445798
Naman Jindal <namanjindal113@gmail.com>
2023-09-23 23:21:12 +0530

    updated

--- JS/debouncing/index.js
+++ JS/debouncing/index.js
@@ -1,13 +1,34 @@
 let timer;
 async function getUserDetails(query){
-    const result = await fetch(`https://dummyjson.com/users/search?q=${query}`);
-    const data = await result.json();
-    console.log(data)
+    try{
+        const result = await fetch(`https://dummyjson.com/users/search?q=${query}`);
+        if(result.ok){
+            const data = await result.json();
+            console.log(data)
+        }
+        else{
+            throw new Error('I am not being able to handle')
+        }
+    }
+    catch(err){
+        console.log(err)
+    }
 }
+const debouncedSearch = debounce2(getUserDetails, 1000);
 
 function handleOnChange(e){
-    
-    debounce(()=>getUserDetails(e.target.value), 1000)
+    //debounce(()=>getUserDetails(e.target.value), 1000)
+    debouncedSearch(e.target.value)
+}
+
+function debounce2(fn, delay){
+    let timer;
+    return function(queryValues){
+        clearTimeout(timer);
+        timer = setTimeout(()=>{
+            fn(queryValues);
+        }, delay)
+    }
 }
 
 function debounce(fn, delay){
@@ -18,3 +39,30 @@ function debounce(fn, delay){
         fn()
     }, delay)
 }
+
+
+// const debouncedSearch = debounce2(getUserDetails, 1000);
+// const debouncedSearch2 = debounce2(getUserDetails, 1000);
+// let toggle = true;
+
+// function handleOnChange(e){
+//     //debounce(()=>getUserDetails(e.target.value), 1000)
+//     debouncedSearch(e.target.value)
+//     if(toggle){
+//         debouncedSearch(e.target.value);
+//     }
+//     else{
+//         debouncedSearch2(e.target.value)
+//     }
+//     toggle = !toggle;
+// }
+
+// function debounce2(fn, delay){
+//     let timer;
+//     return function(queryValues){
+//         clearTimeout(timer);
+//         timer = setTimeout(()=>{
+//             fn(queryValues);
+//         }, delay)
+//     }
+// }
