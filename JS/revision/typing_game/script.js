235308b3afc284f3a44bf27c13d2c6a2f25e0fd5
Naman Jindal <namanjindal113@gmail.com>
2023-10-08 14:30:50 +0530

    typing game phase 1

--- JS/revision/typing_game/script.js
+++ JS/revision/typing_game/script.js
@@ -2,23 +2,26 @@ const API = 'https://api.quotable.io/random';
 const displayQuoteTag = document.querySelector('.displayQuote');
 let currentQuote = [];
 let currentIndexOfQuote = 0;
+let quoteLength;
 let allLetterSpanTags;
 async function getQuote(){
     const result = await fetch(API);
     const data = await result.json();
     currentQuote = data.content;
+    quoteLength = data.length;
     currentIndexOfQuote = 0;
     displayQuote(data.content)
 }
 
 function displayQuote(quote){
-    const container = document.createDocumentFragment();
+    const container = document.createElement('div'); //empty container
     for(let letter of quote){
         const spanTag = document.createElement('span');
         spanTag.textContent = letter;
         container.appendChild(spanTag);
     }
-    displayQuoteTag.appendChild(container);
+    displayQuoteTag.innerHTML = '' //
+    displayQuoteTag.appendChild(container)
     allLetterSpanTags = document.querySelectorAll('span');
 }
 
@@ -32,4 +35,7 @@ window.addEventListener('keypress', (e)=>{
         allLetterSpanTags[currentIndexOfQuote].classList.add('incorrect')
     }
     currentIndexOfQuote++;
+    if(currentIndexOfQuote >= quoteLength){
+        getQuote();
+    }
 })
