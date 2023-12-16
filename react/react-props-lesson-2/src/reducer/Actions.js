5d9219d11b28b0a682145a2ab5d07360ee6a535b
Naman Jindal <namanjindal113@gmail.com>
2023-12-16 12:23:29 +0530

    useReducer payload

--- react/react-props-lesson-2/src/reducer/Actions.js
+++ react/react-props-lesson-2/src/reducer/Actions.js
@@ -1,3 +1,4 @@
 export const INCREMENT = 'increment'
 export const DECREMENT = 'decrement' 
 export const RESET = 'reset' 
+export const INCREMENT_BY_A_RANDOM_NUMBER = 'increament_by_a_random_number'
--- react/react-props-lesson-2/src/reducer/App.js
+++ react/react-props-lesson-2/src/reducer/App.js
@@ -1,5 +1,5 @@
-import React, {useReducer} from 'react'
-import { INCREMENT, DECREMENT, RESET } from './Actions'
+import React, {useReducer, useState} from 'react'
+import { INCREMENT, DECREMENT, RESET, INCREMENT_BY_A_RANDOM_NUMBER } from './Actions'
 
 export default function App() {
 
@@ -11,13 +11,16 @@ export default function App() {
 
     function reducer(state, action){
         console.log('reducer called')
-        switch(action){
+        switch(action.type){
             case INCREMENT:
-                return {count: state.count+ 1}
+                return {...state, count: state.count+ 1}
             case DECREMENT:
-                return {count: state.count- 1}
+                return {...state, count: state.count- 1}
             case RESET:
-                return {count: 0}
+                return {...state, count: 0}
+            case INCREMENT_BY_A_RANDOM_NUMBER:
+                return {...state, count: state.count + action.payload.numb}
+
         }
         //
     }
@@ -26,14 +29,23 @@ export default function App() {
     //     setState((prev)=> {prev+1})
     //     setState((prev)=> {prev+1})
     // }
+    useState({count1: 0, count2: 0});
+    //update count1 only -> then how do you? 
 
-    const [counter, dispatch] = useReducer(reducer, {count: 0});
+    const [counter, dispatch] = useReducer(reducer, {count: 0, count2: 0});
   return (
     <>
-        {counter.count}
-        <button onClick={()=> dispatch(DECREMENT)}>decrement</button>
-        <button onClick={()=> dispatch(RESET)}>reset</button>
-        <button onClick={()=> dispatch(INCREMENT)}>increment</button>
+        <div>
+            
+            {counter.count}
+        </div>
+        <div>
+            {counter.count2}
+        </div>
+        <button onClick={()=> dispatch({type:DECREMENT})}>decrement</button>
+        <button onClick={()=> dispatch({type:RESET})}>reset</button>
+        <button onClick={()=> dispatch({type:INCREMENT})}>increment</button>
+        <button onClick={()=> dispatch({type:INCREMENT_BY_A_RANDOM_NUMBER, payload: {numb: 10}})}>increment by 10</button>
     </>
   )
 }
