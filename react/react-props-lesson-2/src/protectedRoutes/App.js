4b8a713dd177d0dd4986ab3a689f7704d3c56f19
Naman Jindal <namanjindal113@gmail.com>
2023-12-19 22:33:51 +0530

    navbar implementation protected routes

--- react/react-props-lesson-2/src/protectedRoutes/App.js
+++ react/react-props-lesson-2/src/protectedRoutes/App.js
@@ -1,21 +1,70 @@
 import React, { Children } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Outlet, Link } from 'react-router-dom'
 import {userRoleMapping, ROLES} from './Utils/constants'
 
-const LOGGED_IN_USER = userRoleMapping.naman //in reality having the compelte info of the user, currently having just the role directlt
+const LOGGED_IN_USER = userRoleMapping.dinesh //in reality having the compelte info of the user, currently having just the role directlt
 
 export default function App() {
   return (
     <BrowserRouter>
         <Routes>
-            <Route path='/finance' element={<FinanceAuth> <Finance /> </FinanceAuth>} />
-            <Route path='/logistics' element={<Logistics />} />
-            <Route path='/product' element={<Product />} />
-            <Route path='/admin' element={<AdminAuth> <Admin /> </AdminAuth>} />
-            <Route path='*' element={<NotFound />} />
+            <Route path='/' element={<PageLayout />}>
+                <Route path='finance' element={<FinanceAuth> <Finance /> </FinanceAuth>} />
+                <Route path='logistics' element={<LogisticsAuth> <Logistics /> </LogisticsAuth>} />
+                <Route path='product' element={<ProductAuth><Product /></ProductAuth>} />
+                <Route path='admin' element={<AdminAuth> <Admin /> </AdminAuth>} />
+                <Route path='shipments' element={<LogisticsAuth><Shipments /></LogisticsAuth>} />
+                <Route path='*' element={<NotFound />} />
+            </Route>
         </Routes>
     </BrowserRouter>
   )
+}
+function PageLayout(){
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li><Link to={'/finance'}>Finance</Link></li>
+                    {isLogistics() && <li><Link to={'/logistics'}>Logistics</Link></li>}
+                    <li><Link to={'/product'}>Product</Link></li>
+                    <li><Link to={'/admin'}>Admin</Link></li>
+                    <li><Link to={'/shipments'}>Shipments</Link></li>
+                </ul>
+            </nav>
+            <Outlet />
+        </>
+    )
+}
+
+function isLogistics(){
+    if(
+        ROLES.ADMIN === LOGGED_IN_USER || 
+        ROLES.LOGISTICS === LOGGED_IN_USER ||
+        ROLES.SUBADMIN === LOGGED_IN_USER
+    ){
+        return true
+    }
+    return false
+}
+
+function LogisticsAuth({children}){
+    //Only admins and the finance team can access this
+    if( isLogistics()){
+        return (<>{children}</>)
+    }
+    else return <NotAuthorised />
+}
+function ProductAuth({children}){
+    //Only admins and the finance team can access this
+    if( 
+        ROLES.ADMIN === LOGGED_IN_USER || 
+        ROLES.PRODUCT === LOGGED_IN_USER ||
+        ROLES.SUBADMIN === LOGGED_IN_USER
+    ){
+        return (<>{children}</>)
+    }
+    else return <NotAuthorised />
 }
 function FinanceAuth({children}){
     //Only admins and the finance team can access this
@@ -50,6 +99,11 @@ function Product(){
 function Admin(){
     return (
         <>I am the Admin page</>
+    )
+}
+function Shipments(){
+    return (
+        <>I am the Shipments page</>
     )
 }
 function NotFound(){
